784fba6d026201170611c12db8e61c17140db43a
Andrey123815 <ivredomon@gmail.com>
2022-10-04 17:30:21 +0300

    CategoryFilterBlock with states

--- src/UI-KIT/CategoryFilterTab/CategoryFilterTab.jsx
+++ src/UI-KIT/CategoryFilterTab/CategoryFilterTab.jsx
@@ -3,7 +3,10 @@ import './CategoryFilterTab.scss';
 
 function CategoryFilterTab(props) {
   return (
-    <section className="category-filter-tab">
+    <section
+      className={props.active ? "category-filter-tab active" : "category-filter-tab"}
+      onClick={props.onClick}
+    >
       <div className="tab__start-line-mark"></div>
       <div className="tab__filter">{props.filter}</div>
     </section>
--- src/components/CategoryFilterBlock/CategoryFilterBlock.jsx
+++ src/components/CategoryFilterBlock/CategoryFilterBlock.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './CategoryFilterBlock.scss';
 import {FILTERS} from "../../configurations/filters.js";
 import CategoryFilterTab from "../../UI-KIT/CategoryFilterTab/CategoryFilterTab.jsx";
 
+const initFilter = 'Обучение';
+
 function CategoryFilterBlock() {
+  const [currFilter, setCurrFilter] = useState(initFilter);
   return (
     <div className="category-filter-block">
-      {FILTERS.map((filter) => <CategoryFilterTab filter={filter} />)}
+      {FILTERS.map(filter =>
+        <CategoryFilterTab
+          key={filter}
+          filter={filter}
+          active={filter === currFilter}
+          onClick={() => setCurrFilter(filter)}
+        />
+      )}
     </div>
   );
 }
