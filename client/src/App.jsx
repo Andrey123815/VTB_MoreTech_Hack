203085bb0f1a47374efea3fb6a428ba1943b95fc
Andrey123815 <ivredomon@gmail.com>
2022-10-08 18:56:50 +0300

    add marketplace without shopping cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import MainContent from "./components/MainContent/MainContent.jsx";
 import Home from "./screens/Home/Home.jsx";
+import ProfilePopup from "./components/ProfilePopup/ProfilePopup.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <ProfilePopup />
     </div>
   )
 }
--- client/src/components/MainContent/MainContent.jsx
+++ client/src/components/MainContent/MainContent.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './MainContent.scss';
 import TeamMembersLine from "../TeamMembersLine/TeamMembersLine.jsx";
 import TasksBlock from "../TasksBlock/TasksBlock.jsx";
+import Marketplace from "../Marketplace/Marketplace.jsx";
 
 function MainContent() {
+  const isMarket = true;
   return (
     <div className="main-content">
       <TeamMembersLine />
-      <TasksBlock />
+      {isMarket
+        ? <Marketplace />
+        : <TasksBlock />
+      }
     </div>
   );
 }
