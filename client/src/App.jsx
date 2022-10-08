e357bf88868ece6eaff1b0f9bda429e4cd077034
Andrey123815 <ivredomon@gmail.com>
2022-10-08 20:14:05 +0300

    add dynamic structure by height to home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,10 @@
-import './App.css'
-import MainContent from "./components/MainContent/MainContent.jsx";
 import Home from "./screens/Home/Home.jsx";
-import ProfilePopup from "./components/ProfilePopup/ProfilePopup.jsx";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Home />
-    </div>
+    </>
   )
 }
 
